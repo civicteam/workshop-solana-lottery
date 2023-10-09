civicteam/workshop-solana-airdrop

Rudimentary lottery program and client

app/src/LotteryContext.tsx
@@ -1,0 +1,60 @@
+import { createContext, FC, ReactNode, useContext, useEffect, useMemo, useState } from "react";
+import { LotteryClient } from "./lib/LotteryClient";
+import { PublicKey } from "@solana/web3.js";
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { AnchorProvider } from "@project-serum/anchor";
+
+type LotteryContextType = {
+  client: LotteryClient | undefined;
+  createNewLottery: () => void;
+}
+export const LotteryContext = createContext<LotteryContextType>({
+  client: undefined,
+  createNewLottery: () => {},
+});
+
+const safeParsePublicKey = (string: string) => {
+  try {
+    return new PublicKey(string);
+  } catch (e) {
+    return null;
+  }
+};
+
+export const LotteryProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const wallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const [client, setClient] = useState<LotteryClient | undefined>();
+  const addressFromUrl = useMemo(
+    () => safeParsePublicKey(window.location.href.split("#")[1]),
+    [window.location.href]
+  );
+
+  const provider = useMemo(() => {
+    if (!wallet) return undefined;
+
+    return new AnchorProvider(
+      connection,
+      wallet,
+      {}
+    );
+  }, [wallet])
+
+  useEffect(() => {
+    if (!provider || !addressFromUrl) return undefined;
+    LotteryClient.get(provider, addressFromUrl).then(setClient);
+  }, [addressFromUrl, provider]);
+
+  const createNewLottery = () => {
+    if (!provider) return undefined;
+    LotteryClient.create(provider).then(setClient);
+  }
+
+  return (
+    <LotteryContext.Provider value={{ client,createNewLottery }}>
+      {children}
+    </LotteryContext.Provider>
+  );
+}
+
+export const useLottery = () => useContext(LotteryContext);

app/src/lib/LotteryClient.ts
@@ -1,0 +1,92 @@
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionSignature
+} from "@solana/web3.js";
+import { AnchorProvider, IdlAccounts, Program } from "@project-serum/anchor";
+import { IDL, WorkshopSolanaLottery } from "./types/workshop_solana_lottery";
+
+const PROGRAM_ID = new PublicKey("1o1owUQqURMoQY7ydHN6hjqUiQRniHBbgJXb1TMRG2j");
+
+export class LotteryClient {
+  constructor(
+    readonly program: Program<WorkshopSolanaLottery>,
+    readonly authority: PublicKey,
+    readonly lottery: IdlAccounts<WorkshopSolanaLottery>['lottery'],
+    readonly lotteryAddress: PublicKey,
+    readonly ticket: IdlAccounts<WorkshopSolanaLottery>['ticket'] | null,
+    readonly ticketAddress: PublicKey,
+  ) {
+  }
+
+  static async get(provider: AnchorProvider, lotteryAddress: PublicKey): Promise<LotteryClient | undefined> {
+    const program = new Program<WorkshopSolanaLottery>(IDL, PROGRAM_ID, provider);
+    const lottery = await program.account.lottery.fetchNullable(lotteryAddress);
+
+    if (!lottery) return undefined;
+
+    const [ticketAddress, bump] = PublicKey.findProgramAddressSync([
+      lotteryAddress.toBuffer(),
+      provider.publicKey.toBuffer(),
+      Buffer.from("ticket")
+    ], PROGRAM_ID);
+    const ticket = await program.account.ticket.fetchNullable(ticketAddress)
+
+    return new LotteryClient(program, provider.publicKey, lottery, lotteryAddress, ticket, ticketAddress);
+  }
+
+  static async create(provider: AnchorProvider):Promise<LotteryClient> {
+    const program = new Program<WorkshopSolanaLottery>(IDL, PROGRAM_ID, provider);
+    const newLottery = Keypair.generate();
+    await program.methods.initialize().accounts({
+      lottery: newLottery.publicKey,
+      authority: provider.publicKey,
+    }).signers([newLottery]).rpc();
+
+    return LotteryClient.get(provider, newLottery.publicKey).then((client) => {
+      if (!client) throw new Error("Failed to create lottery");
+      return client;
+    });
+  }
+
+  async pickWinner(): Promise<TransactionSignature> {
+    if (!this.authority.equals(this.lottery.authority)) throw new Error("Only the authority can pick a winner");
+
+    return this.program.methods.pickWinner().accounts({
+      lottery: this.lotteryAddress,
+      authority: this.authority,
+    }).rpc();
+  }
+
+  async enter(): Promise<TransactionSignature> {
+    if (this.ticket) throw new Error("You already have a ticket");
+
+    return this.program.methods.enter().accounts({
+        lottery: this.lotteryAddress,
+        ticket: this.ticketAddress,
+        applicant: this.authority,
+    }).rpc();
+  }
+
+  async withdraw(): Promise<TransactionSignature> {
+    return this.program.methods.withdraw().accounts({
+      lottery: this.lotteryAddress,
+      ticket: this.ticketAddress,
+      winner: this.authority,
+    }).rpc();
+  }
+
+  async deposit(lamports: number): Promise<TransactionSignature> {
+    const tx = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: this.authority,
+        toPubkey: this.lotteryAddress,
+        lamports,
+      })
+    );
+
+    return (this.program.provider as AnchorProvider).sendAndConfirm(tx);
+  }
+}

civicteam/workshop-solana-lottery

app/src/LotteryContext.tsx
@@ -1,0 +1,60 @@
+import { createContext, FC, ReactNode, useContext, useEffect, useMemo, useState } from "react";
+import { LotteryClient } from "./lib/LotteryClient";
+import { PublicKey } from "@solana/web3.js";
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { AnchorProvider } from "@coral-xyz/anchor";
+
+type LotteryContextType = {
+  client: LotteryClient | undefined;
+  createNewLottery: () => void;
+}
+export const LotteryContext = createContext<LotteryContextType>({
+  client: undefined,
+  createNewLottery: () => {},
+});
+
+const safeParsePublicKey = (string: string) => {
+  try {
+    return new PublicKey(string);
+  } catch (e) {
+    return null;
+  }
+};
+
+export const LotteryProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const wallet = useAnchorWallet();
+  const { connection } = useConnection();
+  const [client, setClient] = useState<LotteryClient | undefined>();
+  const addressFromUrl = useMemo(
+    () => safeParsePublicKey(window.location.href.split("#")[1]),
+    [window.location.href]
+  );
+
+  const provider = useMemo(() => {
+    if (!wallet) return undefined;
+
+    return new AnchorProvider(
+      connection,
+      wallet,
+      {}
+    );
+  }, [wallet])
+
+  useEffect(() => {
+    if (!provider || !addressFromUrl) return undefined;
+    LotteryClient.get(provider, addressFromUrl).then(setClient);
+  }, [addressFromUrl, provider]);
+
+  const createNewLottery = () => {
+    if (!provider) return undefined;
+    LotteryClient.create(provider).then(setClient);
+  }
+
+  return (
+    <LotteryContext.Provider value={{ client,createNewLottery }}>
+      {children}
+    </LotteryContext.Provider>
+  );
+}
+
+export const useLottery = () => useContext(LotteryContext);

app/src/lib/LotteryClient.ts
@@ -1,0 +1,92 @@
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionSignature
+} from "@solana/web3.js";
+import { AnchorProvider, IdlAccounts, Program } from "@coral-xyz/anchor";
+import { IDL, WorkshopSolanaLottery } from "./types/workshop_solana_lottery";
+
+const PROGRAM_ID = new PublicKey("1o1owUQqURMoQY7ydHN6hjqUiQRniHBbgJXb1TMRG2j");
+
+export class LotteryClient {
+  constructor(
+    readonly program: Program<WorkshopSolanaLottery>,
+    readonly authority: PublicKey,
+    readonly lottery: IdlAccounts<WorkshopSolanaLottery>['lottery'],
+    readonly lotteryAddress: PublicKey,
+    readonly ticket: IdlAccounts<WorkshopSolanaLottery>['ticket'] | null,
+    readonly ticketAddress: PublicKey,
+  ) {
+  }
+
+  static async get(provider: AnchorProvider, lotteryAddress: PublicKey): Promise<LotteryClient | undefined> {
+    const program = new Program<WorkshopSolanaLottery>(IDL, PROGRAM_ID, provider);
+    const lottery = await program.account.lottery.fetchNullable(lotteryAddress);
+
+    if (!lottery) return undefined;
+
+    const [ticketAddress, bump] = PublicKey.findProgramAddressSync([
+      lotteryAddress.toBuffer(),
+      provider.publicKey.toBuffer(),
+      Buffer.from("ticket")
+    ], PROGRAM_ID);
+    const ticket = await program.account.ticket.fetchNullable(ticketAddress)
+
+    return new LotteryClient(program, provider.publicKey, lottery, lotteryAddress, ticket, ticketAddress);
+  }
+
+  static async create(provider: AnchorProvider):Promise<LotteryClient> {
+    const program = new Program<WorkshopSolanaLottery>(IDL, PROGRAM_ID, provider);
+    const newLottery = Keypair.generate();
+    await program.methods.initialize().accounts({
+      lottery: newLottery.publicKey,
+      authority: provider.publicKey,
+    }).signers([newLottery]).rpc();
+
+    return LotteryClient.get(provider, newLottery.publicKey).then((client) => {
+      if (!client) throw new Error("Failed to create lottery");
+      return client;
+    });
+  }
+
+  async pickWinner(): Promise<TransactionSignature> {
+    if (!this.authority.equals(this.lottery.authority)) throw new Error("Only the authority can pick a winner");
+
+    return this.program.methods.pickWinner().accounts({
+      lottery: this.lotteryAddress,
+      authority: this.authority,
+    }).rpc();
+  }
+
+  async enter(): Promise<TransactionSignature> {
+    if (this.ticket) throw new Error("You already have a ticket");
+
+    return this.program.methods.enter().accounts({
+        lottery: this.lotteryAddress,
+        ticket: this.ticketAddress,
+        applicant: this.authority,
+    }).rpc();
+  }
+
+  async withdraw(): Promise<TransactionSignature> {
+    return this.program.methods.withdraw().accounts({
+      lottery: this.lotteryAddress,
+      ticket: this.ticketAddress,
+      winner: this.authority,
+    }).rpc();
+  }
+
+  async deposit(lamports: number): Promise<TransactionSignature> {
+    const tx = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: this.authority,
+        toPubkey: this.lotteryAddress,
+        lamports,
+      })
+    );
+
+    return (this.program.provider as AnchorProvider).sendAndConfirm(tx);
+  }
+}